1f21c5810e8f7ae8c1fdf979bfd10f154809328f
yangli01 <772467757@qq.com>
2020-10-23 17:58:23 +0800

    feat: cli数据获取

--- example/service.js
+++ example/service.js
@@ -1,19 +1,28 @@
 import request from "@/utils/request";
 
-//更改预售商品状态接口
-export function* editAdvanceSellProduct(id, params) {
-  const res = yield request.put(`/mgmt/advanceSellProduct/status/${id}`, {
+//Place an order for a pet
+export function* addOrder(params) {
+  const res = yield request.post("/store/order", {
     params: params
   });
 
   return res;
 }
 
-//添加标品接口
-export function* addStandProduct(params) {
-  const res = yield request.post("/mgmt/standProduct/", {
-    params: params
-  });
+//Find purchase order by ID
+export function* getOrder(orderId) {
+  const res = yield request.get(`/store/order/${orderId}`);
+  return res;
+}
 
+//Delete purchase order by ID
+export function* deleteOrder(orderId) {
+  const res = yield request.delete(`/store/order/${orderId}`);
+  return res;
+}
+
+//Returns pet inventories by status
+export function* getInventory() {
+  const res = yield request.get("/store/inventory");
   return res;
 }
--- src/cli.js
+++ src/cli.js
@@ -1,15 +1,18 @@
 import {
   filterPaths,
 } from '../utils/format'
-import generator from './generator';
+import { changeFile } from '../utils';
+import generator from './recast/services';
 
+const fs = require('fs');
+const path = require('path');
 const request = require('request')
 const qoa = require('qoa');
 // swagger api-docs
-const api = 'http://promo-service-v2.dev.jianke.com/v2/api-docs';
+const api = 'https://petstore.swagger.io/v2/swagger.json';
 const keyword = {
     type: 'input',
-    query: 'Enter your interface keyword:',
+    query: 'Enter your controller keyword:',
     handle: 'key'
   };
 console.log(`try to request ${api}`);
@@ -21,14 +24,22 @@ const cb = async (err, response) => {
     const selectList = tags.filter(i=>i.name.indexOf(kRes.key) !== -1);
     const selectControl = {
       type: 'interactive',
-      query: 'Select your api control:',
+      query: 'Select your api controller:',
       handle: 'control',
       symbol: '>',
       menu: selectList.map(i=>i.name),
     };
     const cRes = await qoa.prompt([selectControl]);
     const p = filterPaths(cRes.control, paths);
-    generator(p);
+
+    const dir = path.join("../example/service.js");
+    const source = fs.readFileSync(dir, "utf8");
+    
+    changeFile(dir, generator(source, p), ()=>{
+        console.log(`成功写入文件${dir}`)
+        console.log(source);
+    });
   }
 }
+
 request(api, cb);
